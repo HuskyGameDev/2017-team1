a964afe0b5363e968438673a3d300855cc7ff5dd
Doogie Warner <dgwarner@mtu.edu>
2018-04-19 20:46:09 -0400

    Update SpeedUp.cs

--- Assets/Scripts/SpeedUp.cs
+++ Assets/Scripts/SpeedUp.cs
@@ -5,27 +5,27 @@ using UnityEngine;
 public class SpeedUp : MonoBehaviour
 {
 	public static float speed = 50f;
-    private void Start()
-    {
-	    timeStart = time;
-        GetComponent<Rigidbody>().velocity = transform.up * speed;
-		setSpeed(2*getSpeed());
-		if(time-timeStart > 10)
-		  resetSpeed();
-    }
+        private void Start()
+        {
+	   float timeStart = time;
+           GetComponent<Rigidbody>().velocity = transform.up * speed;
+           setSpeed(2*getSpeed());
+           if(time-timeStart > 10)
+	      resetSpeed();
+        }
 
 	public static float getSpeed()
 	{
-		return speed;
+	   return speed;
 	}
 
 	public static void setSpeed(float newSpeed)
 	{
-		speed = newSpeed;
+           speed = newSpeed;
 	}
 	
-	public static resetSpeed()
+	public static void resetSpeed()
 	{
-	    speed = 50f;
+	   speed = 50f;
 	}
 }
