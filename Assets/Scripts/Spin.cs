bb6a54089f80c200af36eed39aea7fa92746eff4
Stephen Colegrove <sdcolegr@mtu.edu>
2018-04-24 19:53:39 -0400

    Added spinning animation resources I guess

---
+++ Assets/Scripts/Spin.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+
+public class Spin : MonoBehaviour
+{
+    public float degreesPerSecondx = 10f;
+	public float degreesPerSecondy = 10f;
+	public float degreesPerSecondz = 10f;
+    
+    
+    void Update ()
+    {
+        transform.Rotate(new Vector3(Time.deltaTime * degreesPerSecondx,Time.deltaTime * degreesPerSecondy ,Time.deltaTime * degreesPerSecondz), Space.World);
+    }
+}
