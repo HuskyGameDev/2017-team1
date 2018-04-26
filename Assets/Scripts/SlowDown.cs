4df561f6b11c6f8ff5cc790e489e3b14c3989e60
Doogie Warner <dgwarner@mtu.edu>
2018-04-25 20:05:01 -0400

    Update SlowDown.cs

--- Assets/Scripts/SlowDown.cs
+++ Assets/Scripts/SlowDown.cs
@@ -2,23 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SlowDownSurround : MonoBehaviour
+public class SlowDown : MonoBehaviour
 {
-	public static float speed = 50f;
+  public static float speed = 50f;
   private void Start()
   {
-	    float timeStart = time;
+      float timeStart = Time.time;
       ArrowMover.setSpeed(0.5*speed);
       LogMover.setSpeed(0.5*speed);
       TerrainMover.setSpeed(0.5*speed);
-      if(time-timeStart > 10)
-	      resetSpeed();
+      if(Time.time-timeStart > 10)
+      resetSpeed();
    }
 	
-	public static void resetSpeed()
-	{
+
+   public static void resetSpeed()
+   {
       ArrowMover.setSpeed(speed);
       LogMover.setSpeed(speed);
       TerrainMover.setSpeed(speed);
-	}
+   }
 }
